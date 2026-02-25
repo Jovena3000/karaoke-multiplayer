0db8857888df9aeeddf3a229c62ec7632e137008
Jovena3000 <146227500+Jovena3000@users.noreply.github.com>
2026-02-25 02:45:09 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// ============================================
+// CONFIGURAÇÃO DA API - KARAOKÊ MULTIPLAYER
+// ============================================
+const API_URL = 'https://karaoke-api-backend2-omega.vercel.app';
