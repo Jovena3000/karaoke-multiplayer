d6181681bef32e070e6325d49b3b6a85759599d2
Jovena3000 <146227500+Jovena3000@users.noreply.github.com>
2026-05-17 20:30:49 -0300

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ const CONFIG = {
     
     // Planos disponíveis
     PLANS: {
-        mensal: { name: 'Plano Mensal', price: 5.00 },
-        trimestral: { name: 'Plano Trimestral', price: 49.90 },
-        semestral: { name: 'Plano Semestral', price: 89.90 },
+        mensal: { name: 'Plano Mensal', price: 34.90 },
+        trimestral: { name: 'Plano Trimestral', price: 59.90 },
+        semestral: { name: 'Plano Semestral', price: 99.90 },
         anual: { name: 'Plano Anual', price: 159.90 }
     }
 };
